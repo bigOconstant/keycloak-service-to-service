ba5c02ecc642d36186e449d2b4aad1bd8b473fc1
caleb mccarthy <caleb.a.mccarthy@gmail.com>
2021-05-23 08:05:11 -0500

    Add stuff

--- python/main.py
+++ python/main.py
@@ -5,7 +5,6 @@ import json
 url = os.environ['TOKENURL']
 myobj = {'grant_type': 'client_credentials','client_id':os.environ['CLIENTID'],'client_secret':os.environ['CLIENTSECRET']}
 r = requests.post(url, data = myobj)
-#print('blah', 'blahblah')
 result = r.json()
 print('Token expires in:',result['expires_in'] ,'seconds\n')
 print('Token: ',result['access_token'])
